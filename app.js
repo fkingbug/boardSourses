8fae3bf919a436b0f88440b66bd5cc82dd61aa56
fkingBug <fkingbug@gmail.com>
2021-08-14 20:55:27 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('#board');
+const colors = ['#2b695e', '#546924', '#3498db', '#3b2469', '#692446', '#693b24', '#7d6815'];
+const SQYARES_NUMBER = 500;
+
+for (let i = 0; i < SQYARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => {
+    setColor(square);
+  });
+
+  square.addEventListener('mouseleave', () => {
+    removeColor(square);
+  });
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color} , 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
